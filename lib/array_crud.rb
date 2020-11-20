67d323b6a26b81166569bbb52635ec0296e45443
Amedeo d'Amore <amedeodamore@gmail.com>
2020-11-20 16:32:56 +0000

    commit tree 97d2530fb0ddc63c18280b6abb72dc8c90e70836

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,47 @@
 def create_an_empty_array
+    empty_array = []
   
 end
 
 def create_an_array
-  
+    array_with_4 = ["oreo", "slack", "mac", "microsoft"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+    array_with_4 = ["oreo", "slack", "mac", "microsoft"]
+    array_with_4.push("arrays!")
+    array_with_4
 end
 
 def add_element_to_start_of_array(array, element)
-  
+    array_with_4 = ["oreo", "slack", "mac", "microsoft"]
+    array_with_4.unshift("wow")
+    array_with_4
 end
 
 def remove_element_from_end_of_array(array)
-  
+    array_with_5 = ["oreo", "slack", "mac", "microsoft", "arrays!"]
+    array_with_4 = array_with_5.pop
+    array_with_4
 end
 
 def remove_element_from_start_of_array(array)
-  
+    array_with_5 = ["wow", "slack", "mac", "microsoft", "arrays!"]
+    array_with_4 = array_with_5.shift
+    array_with_4
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+    array_with_5 = ["wow", "mac", "am", "microsoft", "arrays!"]
+    array_with_5[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+    array_with_5 = ["wow", "mac", "am", "microsoft", "arrays!"]
+    array_with_5[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+    array_with_5 = ["wow", "mac", "am", "microsoft", "arrays!"]
+    array_with_5[-1]   
 end
